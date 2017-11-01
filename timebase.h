ed72a4dcab0783f159631a50cdd043606231d9f1
You-Sheng Yang <vicamo@gmail.com>
2017-11-01 11:27:41 +0800

    android: add generated headers
    Configured with:
    scons manbuild=False ncurses=False ntpshm=False python=False qt=False shm_export=False systemd=False usb=False xgps=False

---
+++ timebase.h
@@ -0,0 +1,9 @@
+/*
+ * Constants used for GPS time detection and rollover correction.
+ *
+ * Correct for week beginning 2017-10-26T00:00:00
+ */
+#define BUILD_CENTURY	2000
+#define BUILD_WEEK	949                   # Assumes 10-bit week counter
+#define BUILD_LEAPSECONDS	19
+#define BUILD_ROLLOVERS	1         # Assumes 10-bit week counter
